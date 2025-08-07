d3117679357a05679a63265ed5ac7258e3a3907e
chwl66 <58024471+chwl66@users.noreply.github.com>
2025-08-07 11:18:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -162,7 +162,7 @@ async function handleRootRequest(request, USERNAME, PASSWORD, enableAuth) {
 <body>
   <div class="background" id="background"></div>
   <div class="card">
-      <div class="title">随意文件床</div>
+      <div class="title" style="color:red; font-size: 48px; text-align: center;">随意文件床</div>
       <button type="button" class="btn" id="viewCacheBtn" title="查看历史记录"><i class="fas fa-clock"></i></button>
       <button type="button" class="btn" id="compressionToggleBtn"><i class="fas fa-compress"></i></button>
       <div class="card-body">
